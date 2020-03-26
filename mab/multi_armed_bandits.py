13bda547fe33ce1c69c6452072931908eebb8659
SebastianoF <sebastiano.ferraris@gmail.com>
2020-03-26 19:06:13 +0000

    update

--- mab/multi_armed_bandits.py
+++ mab/multi_armed_bandits.py
@@ -7,4 +7,4 @@ from attr.validators import instance_of
 class MultiArmedBandit:
     K = attr.ib(default=10, validator=instance_of(int))
     mu = attr.ib(default=np.random., validator=instance_of(np.ndarray))
-    sigma =
+    # sigma =
