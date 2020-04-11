2812e89e1e1316886ba583daec75f522fef1dc93
SebastianoF <sebastiano.ferraris@gmail.com>
2020-04-11 17:57:54 +0100

    pre-commit added

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 import os
 
-from setuptools import Command, find_packages, setup
-
 from pip._internal.network.session import PipSession
 from pip._internal.req import parse_requirements
+from setuptools import Command, find_packages, setup
 
 
 class Cleaner(Command):
@@ -19,9 +18,17 @@ with open("README.md") as f:
 requirements = [
     str(requirement).split()[0]
     for requirement in parse_requirements(
-        "requirements.txt", session=PipSession()
+        "requirements/requirements.txt", session=PipSession()
     )
 ]
+
+requirements_dev = [
+    str(requirement).split()[0]
+    for requirement in parse_requirements(
+        "requirements/requirements.txt", session=PipSession()
+    )
+]
+
 
 setup(
     name="multi_armed_bandit",
@@ -31,12 +38,10 @@ setup(
     author="SebastianoF",
     author_email="sebastiano.ferraris@gmail.com",
     url="https://github.com/SebastianoF/multi-armed-bandits-testbed",
-    packages=find_packages(
-        include=["mab"],
-        exclude=["docs", "laboratory", "tests", "examples"],
-    ),
+    packages=find_packages(include=["mab"], exclude=["docs", "laboratory", "examples"]),
     python_requires=">=3.7",
     install_requires=requirements,
+    extras_require={"dev": requirements_dev},
     keywords="multi armed bandit",
     classifiers=[
         "Intended Audience :: Developers/Researchers",
